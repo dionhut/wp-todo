a558dd74bdef68c24b26017174e047955431b0f3
DionHut <dionhut@outlook.com>
2013-09-15 12:03:18 -0700

    fixed typo with userId var should have been user.userId

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,36 +1,36 @@
 function insert(item, user, request) {
-	item.userId = userId;
+	item.userId = user.userId;
     request.execute({
         success: function () {
-            // Write to the response and then send the notification in the background
-            request.respond();
-            // Get the number unchecked items for this user
-            var todoItems = tables.getTable("todoItem");
-            todoItems.where({
-            	userId: user.userId,
-            	Complete: false
-            }).read({
-            	success: function(results) {
-            		if(results.length > 0) {
-            			push.mpns.sendTile(item.channel, {
-            				count: results.length,
-            				title: "Todo"
-            			}, {
-            				success: function(pushResponse) {
-            					console.log("Sent push:", pushResponse);
-            				}
-            			});
-            		} else {
-            			push.mpns.sendTile(item.channel, {
-            				title: "Todo"
-            			}, {
-            				success: function(pushResponse) {
-            					console.log("Sent push (0 count):", pushResponse);
-            				}
-            			});
-            		}
-            	}
-            });
-        }
-    });
+			// Write to the response and then send the notification in the background
+			request.respond();
+			// Get the number unchecked items for this user
+			var todoItems = tables.getTable("todoItem");
+			todoItems.where({
+				userId: user.userId,
+				Complete: false
+			}).read({
+				success: function(results) {
+					if(results.length > 0) {
+						push.mpns.sendTile(item.channel, {
+							count: results.length,
+							title: "Todo"
+						}, {
+							success: function(pushResponse) {
+								console.log("Sent push:", pushResponse);
+							}
+						});
+					} else {
+						push.mpns.sendTile(item.channel, {
+							title: "Todo"
+						}, {
+							success: function(pushResponse) {
+								console.log("Sent push (0 count):", pushResponse);
+							}
+						});
+					}
+				}
+			});
+		}
+	});
 }
