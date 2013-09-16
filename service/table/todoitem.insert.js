cec6262c8cfb2036393d53af835269d6c9a38fe0
DionHut <dionhut@outlook.com>
2013-09-15 20:40:44 -0700

    Debug getting SAS.

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,22 +1,44 @@
+var azure = require('azure'); 
+var qs = require('querystring');
+
 function insert(item, user, request) {
 	item.userId = user.userId;
 
-	if(item.photoName && item.photoName.length > 0) {
-		var azure = require('azure');
-		var blobService = azure.createBlobService("wptodo", "vYX3v/kZcFsgNlVLgkj6prW+fL98a6jIX1ZqTh8YKi7Bmq4V4Ld3QHIp8WD4/mR6XJSTpemosE4nutYDrkQduA==");
-		//create a SAS that expires in an hour
-		var expires = new Date();
-		expires.setMinutes(expires.getMinutes() + 60);
-		var sharedAccessPolicy = { 
-		    AccessPolicy: {
-		        Expiry: expires
-		    }
-		};
-		console.log("sharedAccessPolicy:%j", sharedAccessPolicy);
-		var photoSAS = blobService.getBlobUrl("photos", item.photoName, sharedAccessPolicy);
-		console.log("photoSAS:%j", photoSAS);
-		item.photoSAS = photoSAS.baseUrl + photoSAS.path;
-	}
+    var accountName = 'wptodo';
+    var accountKey = 'vYX3v/kZcFsgNlVLgkj6prW+fL98a6jIX1ZqTh8YKi7Bmq4V4Ld3QHIp8WD4/mR6XJSTpemosE4nutYDrkQduA==';         
+    //var host =   accountName + '.blob.core.windows.net';
+    var container = "photos"
+    var canonicalizedResource = '/' +  container + '/' + item.photoName;
+    
+    //Create the container if it does not exist
+    //we will use public read access for the blobs and will use a SAS to upload        
+    var blobService = azure.createBlobService(accountName, accountKey);
+    blobService.createContainerIfNotExists(item.containerName, {publicAccessLevel : 'blob'}, function(error) {            
+        if(!error){
+            // Container exists now define a policy that provides write access                      
+            // that starts immediately and expires in 5 mins        
+            var sharedAccessPolicy = {
+              AccessPolicy:{
+                    Permissions: azure.Constants.BlobConstants.SharedAccessPermissions.WRITE,
+                    //Start: //use for start time in future, beware of server time skew 
+                    Expiry: formatDate(new Date(new Date().getTime() + 5 * 60 * 1000)) //5 minutes from now
+                }
+            };
+            
+            //Generate the SAS for your BLOB
+            var sasQueryString = getSAS(accountName,
+            	accountKey,
+            	canonicalizedResource,
+            	azure.Constants.BlobConstants.ResourceTypes.BLOB,
+            	sharedAccessPolicy);
+            
+            //full path for resource with sas
+            item.photoSAS =  'https://' + host + canonicalizedResource + '?' + sasQueryString;                                         
+        }
+        else{
+            console.error(error);
+        }
+    });
 
     request.execute({
         success: function () {
@@ -31,4 +53,14 @@ function insert(item, user, request) {
 			});
 		}
 	});
+}
+
+function getSAS(accountName, accountKey, path, resourceType, sharedAccessPolicy) {                         
+	return qs.encode(new azure.SharedAccessSignature(accountName, accountKey).generateSignedQueryString(path, {}, resourceType, sharedAccessPolicy));                           
+}
+
+function formatDate(date){ 
+	var raw = date.toJSON();
+	//blob service does not like milliseconds on the end of the time so strip
+	return raw.substr(0, raw.lastIndexOf('.')) + 'Z';
 }
