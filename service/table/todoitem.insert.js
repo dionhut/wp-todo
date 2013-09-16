327d17079d53580dc795af689f2fbd75723eef56
DionHut <dionhut@outlook.com>
2013-09-15 17:53:10 -0700

    checking if image name is set

--- service/table/todoitem.insert.js
+++ service/table/todoitem.insert.js
@@ -1,16 +1,18 @@
 function insert(item, user, request) {
 	item.userId = user.userId;
 
-	var azure = require('azure');
-	var blobService = azure.createBlobService("wptodo", "vYX3v/kZcFsgNlVLgkj6prW+fL98a6jIX1ZqTh8YKi7Bmq4V4Ld3QHIp8WD4/mR6XJSTpemosE4nutYDrkQduA==");
+	if(item.resourceName && item.resourceName.length > 0) {
+		var azure = require('azure');
+		var blobService = azure.createBlobService("wptodo", "vYX3v/kZcFsgNlVLgkj6prW+fL98a6jIX1ZqTh8YKi7Bmq4V4Ld3QHIp8WD4/mR6XJSTpemosE4nutYDrkQduA==");
 
-	//create a SAS that expires in an hour
-	var sharedAccessPolicy = { 
-	    AccessPolicy: {
-	        Expiry: azure.date.minutesFromNow(60)
-	    }
-	};
-	item.SAS = blobService.getBlobUrl("photos", item.resourceName, sharedAccessPolicy);
+		//create a SAS that expires in an hour
+		var sharedAccessPolicy = { 
+		    AccessPolicy: {
+		        Expiry: azure.date.minutesFromNow(60)
+		    }
+		};
+		item.SAS = blobService.getBlobUrl("photos", item.resourceName, sharedAccessPolicy);
+	}
 
     request.execute({
         success: function () {
